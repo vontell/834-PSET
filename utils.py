17f2e7af1bc4e5c6f86332f5c1f17336eaf69127
vontell <vontell@mit.edu>
2018-03-18 21:47:06 -0400

    Initial impl

---
+++ utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Various utiltiy and testing functions
+"""
+import numpy as np
+from nose.tools import assert_equal, ok_
+from IPython.display import display, HTML, clear_output
+
+def test_ok():
+    """ If execution gets to this point, print out a happy message """
+    try:
+        from IPython.display import display_html
+        display_html("""<div class="alert alert-success">
+        <strong>Tests passed!!</strong>
+        </div>""", raw=True)
+    except:
+        print("Tests passed!!")
+        
+def check_example(param):
+    assert param, "This should be true!"
+    return True
