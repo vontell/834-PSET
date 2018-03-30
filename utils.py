38977b29675022be7da694afca5ea6d99c8da6a0
Michael Feffer <mfeffer@mit.edu>
2018-03-30 18:02:04 -0400

    Merged two pset files

--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@
 Various utiltiy and testing functions
 """
 import numpy as np
+import cv2
+import os
+from numpy_sift import SIFTDescriptor 
 from nose.tools import assert_equal, ok_
 from IPython.display import display, HTML, clear_output
 
@@ -16,7 +19,52 @@ def test_ok():
         </div>""", raw=True)
     except:
         print("Tests passed!!")
+
+def test_patch_entropy_solution(get_patch_entropy):
+    test_probs = [-.1, -0, -.5, -.4, -.3, -1, -0, -.5, -.7]
+    assert get_patch_entropy(test_probs) - 0.3888888888888889 < 0.0001, "Total patch entropy is incorrect"
+    return True
         
 def check_example(param):
     assert param, "This should be true!"
     return True
+
+def return_labels(directoryList, range_size, added_limit, count_limit, patch_size=32):
+    print(added_limit)
+    print("range: " , range_size)
+    print("count: " , count_limit)
+    SD = SIFTDescriptor(patchSize = patch_size)
+
+    #creates initial vector of size 3968
+    sift_pictures = np.asarray([[0 for _ in range(range_size)]])
+    for directory in directoryList:
+        #go through the directories with the relevant images
+        for filename in os.listdir(directory):
+            name = directory + '/' + filename
+            if name[-3:] == 'png' or name[-3:] == 'jpg':
+                image = cv2.imread(name)
+                
+                #transform it to black and white and get features
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                h, w = gray.shape
+                corners = cv2.goodFeaturesToTrack(gray,count_limit,0.01,10)
+
+                corners = np.int0(corners)
+                image_sift_features = np.asarray([]) 
+                count, actually_added = 0, 0
+                
+                #Here we want to get 31 images for our feature vector and then break out of the loop
+                while actually_added < added_limit and count < count_limit:
+                    corner = corners[count][0]
+                    
+                    if patch_size/2 <= corner[1] <= h-patch_size/2 and patch_size/2 <= corner[0] <= w-patch_size/2:
+                        patch = gray[corner[1]-int(patch_size/2):corner[1]+int(patch_size/2), corner[0]-int(patch_size/2):corner[0]+int(patch_size/2)]
+                        sift = SD.describe(patch)
+                        image_sift_features = np.append(image_sift_features, sift)
+                        actually_added += 1
+                    count += 1
+
+                sift_pictures = np.append(sift_pictures, [image_sift_features], axis=0)
+
+    return sift_pictures[1:]
+
